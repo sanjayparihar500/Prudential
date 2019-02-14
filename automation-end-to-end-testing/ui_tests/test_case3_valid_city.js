f0dc2eb73bffff5bbb57db887b8fb34deb7a9ddd
sanjayparihar500 <31448988+sanjayparihar500@users.noreply.github.com>
2019-02-14 18:01:09 +0530

    Update test_case3_valid_city.js

--- automation-end-to-end-testing/ui_tests/test_case3_valid_city.js
+++ automation-end-to-end-testing/ui_tests/test_case3_valid_city.js
@@ -6,13 +6,14 @@ describe('Weather app home page', function() {
     beforeAll(function () {
         homePage = new HomePage();
         browser.waitForAngularEnabled(false);
-        homePage.go();
+
 
     });
 
-    // afterAll(function () {
-    //     browser.driver.close();
-    // });
+    beforeEach(function () {
+        homePage.go();
+    });
+
 
     it('Displays temperature for a valid city name', function() {
         homePage.getSearchBox().sendKeys("Mumbai")
